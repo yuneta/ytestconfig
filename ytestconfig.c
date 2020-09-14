e8dd6b01807aa0fb9aa2fe65b2ea2bd3f1a2b909
Niyamaka <niyamaka@yuneta.io>
2020-09-14 11:17:03 +0200

    v4.2.11

--- ytestconfig.c
+++ ytestconfig.c
@@ -23,7 +23,7 @@
 #define NAME        "ytestconfig"
 #define DOC         "Test a json configuration file"
 
-#define APP_VERSION     "4.2.10"
+#define APP_VERSION     "4.2.11"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
