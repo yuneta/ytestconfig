35ef612973a3d1a88ce949c9e50c5de246344b44
Niyamaka <niyamaka@yuneta.io>
2020-12-17 18:27:09 +0100

    meteré las authz como comando

--- ytestconfig.c
+++ ytestconfig.c
@@ -23,7 +23,7 @@
 #define NAME        "ytestconfig"
 #define DOC         "Test a json configuration file"
 
-#define APP_VERSION     "4.3.3"
+#define APP_VERSION     "4.3.4"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
