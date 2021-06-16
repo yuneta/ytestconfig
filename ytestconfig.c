2eec8a8b7d833fa6a2c7022ef6e20ada98b2865b
Niyamaka <niyamaka@yuneta.io>
2021-06-16 19:24:43 +0200

    v4.14.0 A new feature: node owner, the owner of a (prod/staging/test/dev) node governed by a yuneta agent, WARNING gobj_yuno_factory() api has changed

--- ytestconfig.c
+++ ytestconfig.c
@@ -23,7 +23,7 @@
 #define NAME        "ytestconfig"
 #define DOC         "Test a json configuration file"
 
-#define APP_VERSION     "4.13.3"
+#define APP_VERSION     "4.14.0"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
