2265bab3518a1181d858228ab6a7eb28a6c6fcea
Niyamaka <niyamaka@yuneta.io>
2021-09-16 11:32:42 +0200

    v4.18.1 treedb: set wild conversion to real and integer by default but with warning

--- ytestconfig.c
+++ ytestconfig.c
@@ -23,7 +23,7 @@
 #define NAME        "ytestconfig"
 #define DOC         "Test a json configuration file"
 
-#define APP_VERSION     "4.18.0"
+#define APP_VERSION     "4.18.1"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
