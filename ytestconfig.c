0d942c87f137efdc6e5bffdbd7902ad64758c16b
Niyamaka <niyamaka@yuneta.io>
2021-11-02 09:00:23 +0100

    v4.21.0 remote tty in agent with ycommand

--- ytestconfig.c
+++ ytestconfig.c
@@ -23,7 +23,7 @@
 #define NAME        "ytestconfig"
 #define DOC         "Test a json configuration file"
 
-#define APP_VERSION     "4.20.6"
+#define APP_VERSION     "4.21.0"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
